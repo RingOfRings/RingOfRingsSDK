38060d2f3a73ffe2a1987d2e9f0ad9176baaded8
bans <bans@bansui-MacBookPro.local>
2024-07-13 15:58:07 +0900

    Change mnemonic code like moro,s demo app

--- ringcore/src/main/java/com/hyperring/ringofrings/core/utils/CryptoUtil.kt
+++ ringcore/src/main/java/com/hyperring/ringofrings/core/utils/CryptoUtil.kt
@@ -1,18 +1,18 @@
 package com.hyperring.ringofrings.core.utils
-import org.web3j.crypto.Bip39Wallet
 import org.web3j.crypto.CipherException
-import org.web3j.crypto.Credentials
-import org.web3j.crypto.WalletUtils
-import java.io.File
+import org.web3j.crypto.MnemonicUtils
 import java.io.IOException
+import java.security.SecureRandom
 
 class CryptoUtil {
     companion object {
+        private val secureRandom: SecureRandom = SecureRandom()
         fun generateMnemonic(): String? {
             try {
                 // 니모닉 생성
-                val bip39Wallet: Bip39Wallet = WalletUtils.generateBip39Wallet("yourpassword", File("."))
-                val mnemonic: String = bip39Wallet.getMnemonic()
+                val initialEntropy = ByteArray(16)
+                secureRandom.nextBytes(initialEntropy)
+                val mnemonic = MnemonicUtils.generateMnemonic(initialEntropy)
 
                 // 니모닉 출력
                 println("Generated Mnemonic: $mnemonic")
