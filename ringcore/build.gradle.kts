7efadc8d442bd4c2e572525da6b93b8bc920c01b
bans <bans@bansui-MacBookPro.local>
2024-07-15 18:32:57 +0900

    Add crypted shared preferences settings, init sharedpreferences

--- ringcore/build.gradle.kts
+++ ringcore/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
+    implementation("androidx.security:security-crypto-ktx:1.1.0-alpha03")
     implementation("org.web3j:core:4.8.9-android")
     implementation("com.google.crypto.tink:tink-android:1.6.1")
     implementation("com.google.crypto.tink:tink:1.6.1")
--- ringcore/src/main/java/com/hyperring/ringofrings/core/RingCore.kt
+++ ringcore/src/main/java/com/hyperring/ringofrings/core/RingCore.kt
@@ -1,11 +1,20 @@
 package com.hyperring.ringofrings.core
 
-import NetworkUtil
 import android.content.Context
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
 import com.hyperring.ringofrings.core.utils.CryptoUtil
 
 class RingCore {
+    companion object {
+        private const val FILE_NAME = "ring_of_rings"
+        var sharedPrefs : EncryptedSharedPreferences? = null
+    }
+
     fun createWallet(context: Context) {
+        initSharedPrefs(context)
+
 //        // Check Network ???
 //        var isNetworkAvailable = NetworkUtil.isNetworkAvailable(context)
 //        if(!isNetworkAvailable) {
@@ -15,6 +24,23 @@ class RingCore {
         var mnemonic = CryptoUtil.generateMnemonic()
         // Get Private Key
         // Get Public Key
+    }
+
+    private fun initSharedPrefs(context: Context) {
+        if(sharedPrefs == null) {
+            val masterKeyAlias = MasterKey
+                .Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+                .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                .build()
+
+            EncryptedSharedPreferences.create(
+                context,
+                FILE_NAME,
+                masterKeyAlias,
+                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            )
+        }
     }
 
     fun importWalletAddress() {
