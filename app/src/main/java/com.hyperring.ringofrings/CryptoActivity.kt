a775b673b038e9c420dfabda3efc5efde2c6fad8
bans <bans@bansui-MacBookPro.local>
2024-07-28 20:09:51 +0900

    Show List from api

--- app/src/main/java/com.hyperring.ringofrings/CryptoActivity.kt
+++ app/src/main/java/com.hyperring.ringofrings/CryptoActivity.kt
@@ -3,16 +3,23 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModel
@@ -22,12 +29,13 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewModelScope
 import com.hyperring.ringofrings.core.utils.alchemy.AlchemyApi
 import com.hyperring.ringofrings.core.utils.alchemy.data.BalancesJsonBody
+import com.hyperring.ringofrings.core.utils.alchemy.data.TokenBalance
+import com.hyperring.ringofrings.core.utils.alchemy.data.TokenBalanceResult
 import com.hyperring.ringofrings.core.utils.alchemy.data.TokenBalances
 import com.hyperring.ringofrings.ui.theme.RingOfRingsTheme
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -75,13 +83,24 @@ class CryptoActivity : ComponentActivity() {
 
 @Composable
 fun CryptoList(viewModel: CryptoViewModel) {
-    val items = List(viewModel.uiState.collectAsState().value.tokens?.result?.tokenBalances?.size?:0) { "Item #$it" }
+    var tokenBalances = viewModel.items.collectAsState()
 
-    Column{
-        if(items.isEmpty()) BasicText(text = "Empty!", modifier = Modifier.padding(16.dp))
-        else BasicText(text = items[0], modifier = Modifier.padding(16.dp))
+    if (tokenBalances.value.isEmpty()) BasicText(text = "Empty!", modifier = Modifier.padding(16.dp))
+    else LazyColumn(modifier = Modifier.padding(16.dp)) {
+        items(tokenBalances.value.size) { item ->
+            Box(modifier = Modifier
+                .background(if(item % 2 != 0) Color.Gray else Color.LightGray)
+                .padding(20.dp)
+                .fillMaxWidth()
+                ) {
+                Column {
+                    BasicText(text = "[Token Balances]\n${tokenBalances.value.get(item).tokenBalances}", modifier = Modifier.padding(16.dp))
+                    BasicText(text = "[Contract Address]\n${tokenBalances.value.get(item).contractAddress}", modifier = Modifier.padding(16.dp))
+                }
+            }
         }
     }
+}
 
 data class CryptoUiState(
     var tokens: TokenBalances? = null,
@@ -89,9 +108,11 @@ data class CryptoUiState(
 }
 
 class CryptoViewModel : ViewModel() {
-    private val _uiState = MutableStateFlow(CryptoUiState())
-    val uiState: StateFlow<CryptoUiState> = _uiState.asStateFlow()
+    val _uiState = MutableStateFlow(CryptoUiState())
+    val uiState: StateFlow<CryptoUiState> = _uiState
 
+    private val _items = MutableStateFlow<List<TokenBalance>>(emptyList())
+    val items: StateFlow<List<TokenBalance>> = _items
 
     init {
         fetchItems()
@@ -112,6 +133,7 @@ class CryptoViewModel : ViewModel() {
                     Log.d("token result", "result${result.isSuccessful}")
                     Log.d("token result", "result${result.body()}")
                     uiState.value.tokens = result.body()
+                    _items.value = result.body()?.result?.tokenBalances?: emptyList()
                 }
 
                 for (tokenBalance in uiState.value.tokens?.result?.tokenBalances!!) {
