edf7d5987fda2af2c1e0c880a36f87b8aa24549e
bans <bans@bansui-MacBookPro.local>
2024-07-28 20:11:42 +0900

    Add Token Metadata Body for Retrofit

--- ringcore/src/main/java/com/hyperring/ringofrings/core/utils/alchemy/data/BalancesJsonBody.kt
+++ ringcore/src/main/java/com/hyperring/ringofrings/core/utils/alchemy/data/BalancesJsonBody.kt
@@ -6,11 +6,10 @@ data class BalancesJsonBody(
     val method: String = "alchemy_getTokenBalances",
     val params: List<String>
 )
-//    {
-//        "id": 1,
-//        "jsonrpc": "2.0",
-//        "method": "alchemy_getTokenBalances",
-//        "params": [
-//        "0x607f4c5bb672230e8672085532f7e901544a7375"
-//        ]
-//    }
+
+data class TokenMetadataJsonBody(
+    val id: Int = 1,
+    val jsonrpc : String = "2.0",
+    val method: String = "alchemy_getTokenMetadata",
+    val params: List<String>
+)
